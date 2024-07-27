06efd654b697c0952a7311770d6daa5c0b10c6bd
Motasem Ghozlan <Motasem.go.26@gmail.com>
2024-07-27 18:47:05 +0300

    Solutions

--- src/components/pages/home/solutionsSection.tsx
+++ src/components/pages/home/solutionsSection.tsx
@@ -1,4 +1,12 @@
 import { Divider } from "@/components/layout/divider/divider";
+import Image from "next/image";
+
+import blockchainIcon from "../../../app/assets/pages/home/solutions/vac-blockchain.svg";
+import decentralizedStorageIcon from "../../../app/assets/pages/home/solutions/vac-decentralized-storage.svg";
+import metahealthIcon from "../../../app/assets/pages/home/solutions/vac-metahealth.svg";
+import nftIcon from "../../../app/assets/pages/home/solutions/vac-nft.svg";
+import insuranceIcon from "../../../app/assets/pages/home/solutions/vac-insurance.svg";
+import supplyChainIcon from "../../../app/assets/pages/home/solutions/vac-supply-chain.svg";
 
 export function SolutionsSection() {
 
@@ -9,5 +17,23 @@ export function SolutionsSection() {
             <Divider />
         </div>
 
+        <section className="grid gap-8  grid-cols-2 md:grid-cols-3 lg:grid-cols-6 items-end mt-16">
+            <Solution title="VAC Blockgain" image={blockchainIcon} />
+            <Solution title="VAC Decentralized Storage" image={decentralizedStorageIcon} />
+            <Solution title="VAC METAHEALTH" image={metahealthIcon} />
+            <Solution title="VAC NFT" image={nftIcon} />
+            <Solution title="VAC Supply Chain" image={supplyChainIcon} />
+            <Solution title="VAC Insurance" image={insuranceIcon} />
+        </section>
     </section>
+}
+
+export function Solution({ title, image }: { title: string, image: any }) {
+
+    return <article>
+        <div className="w-full lg:px-8 inline-flex justify-center">
+            <Image alt={title} {...image} />
+        </div>
+        <h4 className="text-xs font-extralight text-center mt-2">{title}</h4>
+    </article>
 }
