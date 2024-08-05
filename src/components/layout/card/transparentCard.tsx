fdbc6595a2a0734db9d10e693b4aa3acc1389b03
Motasem Ghozlan <Motasem.go.26@gmail.com>
2024-08-05 04:02:06 +0300

    Counter animation

--- src/components/layout/card/transparentCard.tsx
+++ src/components/layout/card/transparentCard.tsx
@@ -1,13 +1,19 @@
 import { ReactNode } from "react";
+import { Divider } from "../divider/divider";
 
 export interface TransparentCardProps {
     className?: string;
     children?: ReactNode;
+    showLightingPoint?: boolean;
 }
 
-export function TransparentCard({ children, className = '' }: TransparentCardProps) {
+export function TransparentCard({ children, showLightingPoint = false, className = '' }: TransparentCardProps) {
 
-    return <article className={`border border-primary rounded-lg bg-black bg-opacity-10 backdrop-blur-xl ${className}`}>
-        {children}
-    </article>
+    return <>
+        <article className={`transition-all border border-primary rounded-lg bg-black bg-opacity-10 backdrop-blur-xl box-border group ${className}`}>
+            {children}
+            {showLightingPoint && <Divider className="transition-all absolute -bottom-0.5 left-1/4 right-1/4 w-1/2 z-10 
+            group-hover:-translate-x-1/4" />}
+        </article>
+    </>
 }
--- src/components/pages/home/privateSaleCounter.tsx
+++ src/components/pages/home/privateSaleCounter.tsx
@@ -9,22 +9,24 @@ import { DateTime } from "luxon";
 export function PrivateSaleCounter({ isVisible }: ElementVisibilityCheckerComponentProps) {
 
     return <div className={`relative ${isVisible ? 'animate-fade-in' : 'animate-fade-out'}`}>
-        <TransparentCard className="hidden md:block absolute -top-12 -right-5 h-28 w-20 animate-moving-around " />
-        <TransparentCard className="hidden md:block absolute -bottom-12 -left-5 h-40 w-56 -z-20 animate-moving-around" />
-        <TransparentCard className="transition-all relative p-8 w-full z-10 hover:scale-105 hover:bg-opacity-60 bg-opacity-30">
-            <h2 className="text-on-primary text-xl font-bold">PRIVATE SALE WILL BE LAUNCHED AFTER:</h2>
-            <div className="relative grid md:grid-cols-2 gap-12 py-4 z-10">
-                <div>
-                    <p className="text-xs font-extralight">Discount for early investors</p>
-                    <Counter className="mt-6" targetedDate={DateTime.fromSQL('2024-12-12')} />
-                </div>
-                <div className="ms-auto max-w-sm flex flex-col justify-between gap-4">
-                    <p className="text-xs font-extralight">Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor</p>
-
+        <TransparentCard className="hidden md:block !absolute -top-12 -right-5 h-28 w-20 animate-moving-around " />
+        <TransparentCard className="hidden md:block !absolute -bottom-12 -left-5 h-40 w-56 -z-20 animate-moving-around" />
+        <TransparentCard showLightingPoint className="transition-all relative w-full z-10 hover:scale-105 hover:bg-opacity-60 bg-opacity-30">
+            <div className="p-8">
+                <h2 className="text-on-primary text-xl font-bold">PRIVATE SALE WILL BE LAUNCHED AFTER:</h2>
+                <div className="relative grid md:grid-cols-2 gap-12 py-4 z-10">
                     <div>
-                        <VaccineButtonLink className="!w-full block text-center border border-primary" size="xl" buttonType="light" href={'/lightpaper'}>
-                            Join Now
-                        </VaccineButtonLink>
+                        <p className="text-xs font-extralight">Discount for early investors</p>
+                        <Counter className="mt-6" targetedDate={DateTime.fromSQL('2024-12-12')} />
+                    </div>
+                    <div className="ms-auto max-w-sm flex flex-col justify-between gap-4">
+                        <p className="text-xs font-extralight">Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor</p>
+
+                        <div>
+                            <VaccineButtonLink className="!w-full block text-center border border-primary" size="xl" buttonType="light" href={'/lightpaper'}>
+                                Join Now
+                            </VaccineButtonLink>
+                        </div>
                     </div>
                 </div>
             </div>
