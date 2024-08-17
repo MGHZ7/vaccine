9fbdce015c3ee538590d6a84517c88799d5dd597
Motasem Ghozlan <Motasem.go.26@gmail.com>
2024-08-17 12:47:43 +0300

    Insurance card finalizing

--- src/components/pages/project/pricing/insuranceCards/insuranceCards.tsx
+++ src/components/pages/project/pricing/insuranceCards/insuranceCards.tsx
@@ -6,6 +6,7 @@ import gold from "../assets/cards/golden-card.png";
 import silver from "../assets/cards/silver-card.png";
 import bronze from "../assets/cards/bronze-card.png";
 import Image from "next/image";
+import { Paragraph } from "@/components/layout/typography/paragraph";
 
 export function InsuranceCardsSection() {
 
@@ -19,5 +20,20 @@ export function InsuranceCardsSection() {
             <Image alt="Vac Silver Card" {...silver} />
             <Image alt="Vac Bronze Card" {...bronze} />
         </section>
+
+        <article className="max-w-md space-y-4">
+            <Paragraph>This plan may suit your needs...... <span className="text-primary">Coming Soon</span></Paragraph>
+            <Paragraph>Here’s a rundown of ve reasons why it’s important to seek on purchasing our VAC medical insurance plan, VAC insurance card will provide you with:
+            </Paragraph>
+
+            <ul className="text-xs text-primary">
+                <li>- Worldwide coverage</li>
+                <li> - Lowest medical insurance cost</li>
+                <li> - Highest medical coverage</li>
+                <li> - Discount from our partners</li>
+                <li> - Support 24/7 </li>
+                <li> and much more......</li>
+            </ul>
+        </article>
     </section>
 }
