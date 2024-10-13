60edfb3198310a7079e86da79cbe07e12539ab1c
Motasem Ghozlan <Motasem.go.26@gmail.com>
2024-10-13 22:34:34 +0300

    Project pricing responsive

--- src/components/pages/project/pricing/pricingPlansSection.tsx
+++ src/components/pages/project/pricing/pricingPlansSection.tsx
@@ -1,8 +1,12 @@
 import { ElementVisibilityCheckerComponentProps } from "@/components/common/elementInViewPort";
 import { TransparentCard } from "@/components/layout/card/transparentCard";
 import { PricingPlan, PricingPlanProps } from "./pricingPlan";
+import { useScreenSize } from "@/hooks/screen/screenSize.hook";
+import { ScreenSizes } from "@/hooks/screen/screenSize";
 
 export function PricingPlansSection({ isVisible }: ElementVisibilityCheckerComponentProps) {
+
+    const width = useScreenSize();
 
     return <section className={` ${isVisible ? 'animate-fade-in' : 'animate-fade-out'}`}>
         <h2 className="text-2xl font-bold">
@@ -12,12 +16,12 @@ export function PricingPlansSection({ isVisible }: ElementVisibilityCheckerCompo
         </h2>
         <p className="text-xs text-primary">More details</p>
 
-        <article className="p-8">
+        <article className="pt-8 md:mt-0 md:p-8">
             <div className={`relative ${isVisible ? 'animate-fade-in' : 'animate-fade-out'} mt-8`}>
                 <TransparentCard className="hidden md:block !absolute -top-12 -right-5 h-28 w-20 animate-moving-around " />
                 <TransparentCard className="hidden md:block !absolute -bottom-12 -left-5 h-40 w-56 -z-20 animate-moving-around" />
-                <TransparentCard showLightingPoint className="transition-all relative w-full z-10 hover:scale-105 hover:bg-opacity-60 bg-opacity-50">
-                    <div className="p-8">
+                <TransparentCard showLightingPoint={width > ScreenSizes.sm} className="transition-all relative w-full z-10 hover:scale-105 hover:bg-opacity-60 bg-opacity-50">
+                    <div className="p-4 md:p-8">
                         <div className="grid md:grid-cols-2 items-end">
                             <div>
                                 <h3 className="text-on-primary text-3xl font-bold">Choose Your Plan</h3>
--- src/components/pages/project/pricing/vacTokenSection.tsx
+++ src/components/pages/project/pricing/vacTokenSection.tsx
@@ -6,8 +6,9 @@ import image from "./assets/token.png";
 
 export function VacTokenUtility({ isVisible }: ElementVisibilityCheckerComponentProps) {
 
-    return <section className={`grid lg:grid-cols-2 gap-8 justify-center items-end ${isVisible ? 'animate-fade-in' : 'animate-fade-out'}`}>
-        <article className="p-8">
+    return <section className={`grid lg:grid-cols-2 gap-8 justify-center items-end 
+        ${isVisible ? 'animate-fade-in' : 'animate-fade-out'}`}>
+        <article className="md:p-8">
             <h2 className="text-3xl text-on-primary font-bold">VAC Token Utiliy</h2>
             <p className="text-xs text-primary">Lorem ipsum dolor sit amet, consectetur adipiscing</p>
             <Paragraph className="mt-8">
