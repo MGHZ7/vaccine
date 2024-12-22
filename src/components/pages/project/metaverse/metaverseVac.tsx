05c32e30dc1eeb3712cb6cd5f5e1b743acf7cf11
Motasem Ghozlan <Motasem.go.26@gmail.com>
2024-12-22 04:59:14 +0300

    Put vac hospital image above the text

--- src/components/pages/project/metaverse/metaverseVac.tsx
+++ src/components/pages/project/metaverse/metaverseVac.tsx
@@ -8,7 +8,7 @@ export function MetaverseVac() {
 
     return <section className="grid lg:grid-cols-2 items-center mx-auto px-4 md:px-12">
 
-        <article>
+        <article className="order-1 lg:order-0">
             <div className="mb-8">
                 <Heading showDivider>VAC Meta Hospital</Heading>
             </div>
@@ -16,7 +16,7 @@ export function MetaverseVac() {
                 Welcome to the VAC Medical Hospital Metaverse, where the future of healthcare unfolds in a boundless digital landscape. Step through the virtual doors of our state-of-the-art facility, where patients and healthcare professionals converge in a seamless fusion of technology and compassion. Within this immersive environment, individuals can access a comprehensive range of medical services, from routine check-ups to complex surgeries, all from the comfort of their own homes. Our hospital&apos;s virtual wards are staffed by a team of expert physicians, nurses, and specialists, each committed to delivering personalized care and support. Whether you&apos;re seeking treatment for a chronic condition, exploring innovative therapies, or simply looking to optimize your well-being, our Medical Hospital Metaverse offers a transformative healthcare experience like no other. With its emphasis on accessibility, efficiency, and patient-centered care, our virtual hospital is poised to revolutionize the way we approach health and healing in the digital age. Welcome to the future of medicine—welcome to the Medical Hospital Metaverse.
             </Paragraph>
         </article>
-        <Image alt="meta Health" {...image} />
+        <Image className="order-0 lg:order-1" alt="meta Health" {...image} />
 
     </section>
 }
